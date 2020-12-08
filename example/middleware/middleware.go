8855269ae6b36ac82e64e5cba616b51374af4bed
Helloworld <1326521+LYZhelloworld@users.noreply.github.com>
2020-12-08 16:30:18 +0800

    Add middleware example

---
+++ example/middleware/middleware.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/LYZhelloworld/gateway"
+)
+
+// This example shows how the middleware works.
+//
+// The middleware 1 will be executed, and then middleware 2 (before Next()), middleware 3, main handler,
+// then the middleware 2 (after Next()).
+//
+// If you change `context.Next()` to `context.Interrupt()`, the execution will stop at "middleware 2 end" and
+// the following handlers will not be run.
+func main() {
+	s := gateway.Default()
+	cfg := gateway.Config{}
+	cfg.Add("/hello", http.MethodGet, "api.gateway.hello")
+	s.UseConfig(cfg)
+	s.Register("api.gateway.hello", func(context *gateway.Context) {
+		context.Logger.Info("body start")
+		context.Response = []byte("hello, world")
+		context.Logger.Info("body end")
+	})
+	s.UseMiddlewares(func(context *gateway.Context) {
+		context.Logger.Info("middleware 1")
+	}, func(context *gateway.Context) {
+		context.Logger.Info("middleware 2 start")
+		context.Next() // Change this to `context.Interrupt()` to get different result.
+		context.Logger.Info("middleware 2 end")
+	}, func(context *gateway.Context) {
+		context.Logger.Info("middleware 3")
+	})
+	if err := s.RunWithShutdown(":8080", 5*time.Second); err != nil {
+		fmt.Println(err)
+	}
+}
